6159a18da23d9f5fb2a9a95506d09f107f4b794c
Samuel Ogboye <ogboyesam@gmail.com>
2024-06-26 00:30:32 +0100

    feat(transac): updated the transaction controller

--- src/models/transactionModel.ts
+++ src/models/transactionModel.ts
@@ -1,26 +1,28 @@
 import knex from "../utils/db";
 import { Transaction } from "../types";
 
-// Function to create a new transaction
+// Function to create a transaction
 export const createTransaction = async (
   transaction: Partial<Transaction>
 ): Promise<number> => {
-  const [transactionId] = await knex("transactions").insert(transaction);
-  return transactionId;
+  const [id] = await knex("transactions").insert(transaction);
+  return id;
 };
 
-// Function to get a transaction by ID
-export const getTransactionById = async (id: number): Promise<Transaction> => {
-  const transaction = await knex("transactions").where({ id }).first();
-  return transaction;
-};
-
-// Function to get all transactions for a user
+// Function to get transactions by user ID
 export const getTransactionsByUserId = async (
   userId: number
 ): Promise<Transaction[]> => {
-  const transactions = await knex("transactions").where({ user_id: userId });
-  return transactions;
+  return await knex("transactions")
+    .where({ user_id: userId })
+    .orderBy("created_at", "desc");
+};
+
+// Function to get a transaction by ID
+export const getTransactionById = async (
+  transactionId: number
+): Promise<Transaction | null> => {
+  return await knex("transactions").where({ id: transactionId }).first();
 };
 
 // Function to get all transactions for a user with pagination
@@ -34,4 +36,26 @@ export const getPaginatedTransactionsByUserId = async (
     .limit(limit)
     .offset(offset);
   return transactions;
+};
+
+// Function to check for duplicate transactions within 30 seconds
+export const isDuplicateTransaction = async (
+  userId: number,
+  recipientId: number,
+  money_out: number,
+  type: string
+): Promise<boolean> => {
+  const lastTransaction = await knex("transactions")
+    .where({ user_id: userId, recipient_id: recipientId, money_out, type })
+    .orderBy("created_at", "desc")
+    .first();
+
+  if (lastTransaction) {
+    const lastTransactionTime = new Date(lastTransaction.created_at).getTime();
+    const currentTime = new Date().getTime();
+
+    return currentTime - lastTransactionTime <= 30000; // 30 seconds in milliseconds
+  }
+
+  return false;
 };
--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -1,20 +1,30 @@
 import { Router } from "express";
 import {
-  createTransactionController,
+  fundAccountController,
   getTransactionHistory,
+  transferController,
+  withdrawController,
 } from "../controllers/transactionController";
 import { validateTransaction } from "../utils/validator";
 import { authenticate } from "../middlewares/authMiddleware"; // Assuming you have an authentication middleware
 
 const router = Router();
 
-// Route to create a new transaction
-router.post(
-  "/",
-  authenticate,
-  validateTransaction,
-  createTransactionController
-);
+// // Route to create a new transaction
+// router.post(
+//   "/",
+//   authenticate,
+//   validateTransaction,
+//   createTransactionController
+// );
+// Route to fund an account
+router.post("/fund", authenticate, fundAccountController);
+
+// Route to transfer funds
+router.post("/transfer", authenticate, transferController);
+
+// Route to withdraw funds
+router.post("/withdraw", authenticate, withdrawController);
 
 // Route to get transaction history
 router.get("/", authenticate, getTransactionHistory);
