df6e1f9990e5c68e94d9257efc714427b5aea2f1
Samuel Ogboye <ogboyesam@gmail.com>
2024-06-26 00:31:56 +0100

    feat(log): implemented logger system

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler";
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
 import transactionRoutes from "./routes/transactionRoutes";
 import adminRoutes from "./routes/adminRoutes";
+// import logger from "./middlewares/logEvents";
 
 const app = express();
+
+// custom middleware logger
+// app.use(logger);
+
+// Cross Origin Resource Sharing
+// app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use("/api/v1/users", userRoutes);
---
+++ src/middlewares/logEvents.ts
@@ -0,0 +1,30 @@
+import { format } from "date-fns";
+import { v4 as uuid } from "uuid";
+import fs from "fs";
+import fsPromises from "fs/promises";
+import path from "path";
+import { LogEvents, Logger } from "../types";
+
+const logEvents: LogEvents = async (message, logName) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+  try {
+    const logsDir = path.join(__dirname, "..", "logs");
+    if (!fs.existsSync(logsDir)) {
+      await fsPromises.mkdir(logsDir);
+    }
+
+    await fsPromises.appendFile(path.join(logsDir, logName), logItem);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const logger: Logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+export { logger, logEvents };
