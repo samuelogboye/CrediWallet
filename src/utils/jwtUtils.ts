a9fbc0ed1e565c6888667b5ba7492b8f4f2305d5
Samuel Ogboye <ogboyesam@gmail.com>
2024-06-26 21:30:10 +0100

    feat(jwt): set up token util functions

---
+++ src/utils/jwtUtils.ts
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+import logger from "src/config/logger";
+
+// Function to sign a JWT token
+export const signToken = (
+  payload: object,
+  expiresIn: string = "1h"
+): string => {
+  logger.info("jwtUtils.signToken(): Signing token");
+  const token = jwt.sign(payload, config.jwtSecretKey, { expiresIn });
+  logger.info("jwtUtils.signToken(): Token signed successfully");
+  return token;
+};
+
+// Function to decode a JWT token
+export const decodeToken = (token: string): any => {
+  try {
+    logger.info("jwtUtils.decodeToken(): Decoding token");
+    const decoded = jwt.verify(token, config.jwtSecretKey);
+    logger.info("jwtUtils.decodeToken(): Token decoded successfully");
+    return decoded;
+  } catch (error) {
+    const err = error as Error;
+    logger.error(
+      `jwtUtils.decodeToken(): Error decoding token - ${err.message}`
+    );
+    throw new Error("Invalid or expired token");
+  }
+};
