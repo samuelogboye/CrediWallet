1964874a8ecebda95a9da2ca1ddb531cacd9f9b3
Samuel Ogboye <ogboyesam@gmail.com>
2024-06-26 21:27:37 +0100

    refac(logger): refactored logging in auth and separated token utils

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -5,13 +5,18 @@ import { getUserById } from "../models/userModel";
 import ApiError from "./errorHandler";
 import config from "../config/config";
 import { AuthenticatedRequest, SafeUser } from "src/types";
+import logger from "src/config/logger";
+import { decodeToken } from "src/utils/jwtUtils";
 
 export const checkBlacklist = async (
   req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ) => {
+  logger.info("authMiddleware.checkBlacklist(): Function Entry");
+
   const { email } = req.body;
+  logger.info(`Checking blacklist status for email: ${email}`);
 
   try {
     const response = await axios.get(`${config.adjutor.apiUrl}/${email}`, {
@@ -21,6 +26,7 @@ export const checkBlacklist = async (
     });
 
     if (response.status === 200 || response.status === 201) {
+      logger.warn(`Email ${email} is blacklisted`);
       return res.status(403).json({
         message:
           "You cannot register an account with us currently. Please contact support.",
@@ -33,9 +39,15 @@ export const checkBlacklist = async (
       error.response.status === 404 &&
       error.response.data.message === "Identity not found in karma"
     ) {
+      logger.info(`Email ${email} is not blacklisted`);
       return next();
     }
+    logger.error(
+      `Error checking blacklist for email ${email}: ${error.message}`
+    );
     res.status(500).json({ message: "Error checking blacklist", error });
+  } finally {
+    logger.info("authMiddleware.checkBlacklist(): Function Exit");
   }
 };
 
@@ -44,34 +56,41 @@ export const authenticate = async (
   res: Response,
   next: NextFunction
 ) => {
-  const authHeader = req.headers["authorization"];
+  logger.info("authMiddleware.authenticate(): Function Entry");
 
+  const authHeader = req.headers["authorization"];
   if (!authHeader) {
+    logger.warn("Authorization header missing");
     return next(new ApiError(401, "Unauthorized Access"));
   }
 
   const token = authHeader.split(" ")[1];
-
   if (!token) {
+    logger.warn("Token missing in authorization header");
     return next(new ApiError(401, "Token missing"));
   }
 
   try {
-    const decoded: any = jwt.verify(token, config.jwtSecretKey);
+    const decoded = decodeToken(token);
+    logger.info(`Token verified, decoding user ID: ${decoded.userId}`);
 
-    // Optionally, retrieve and attach the user object to the AuthenticatedRequest
     const user = await getUserById(decoded.userId);
-
     if (!user) {
+      logger.warn(`User with ID ${decoded.userId} not found`);
       return next(new ApiError(404, "User not found"));
     }
 
     // Omit the password field
     const { password, ...safeUser } = user;
-
     req.user = safeUser as SafeUser;
+
+    logger.info(`User with ID ${decoded.userId} authenticated successfully`);
     next();
   } catch (error) {
+    const err = error as Error;
+    logger.error(`Error during authentication: ${err.message}`);
     return next(new ApiError(401, "Invalid or expired token"));
+  } finally {
+    logger.info("authMiddleware.authenticate(): Function Exit");
   }
 };
