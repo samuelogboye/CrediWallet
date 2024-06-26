4e9e1edbd02d41459e020610f96072c82019a0c5
Samuel Ogboye <ogboyesam@gmail.com>
2024-06-26 21:26:54 +0100

    refac(logger): refactored logging in user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -10,6 +10,7 @@ import ApiError from "../middlewares/errorHandler";
 import { AuthenticatedRequest } from "../types";
 import { allowedFields } from "src/models/userSchema";
 import { validateUpdateFields } from "src/utils/validator";
+import logger from "src/config/logger";
 
 // Controller to get user details
 export const getUserDetailsController = async (
@@ -17,16 +18,27 @@ export const getUserDetailsController = async (
   res: Response,
   next: NextFunction
 ) => {
+  logger.info("userController.getUserDetailsController(): Function Entry");
+
   const userId = req.user.id;
   try {
+    logger.info(`Fetching details for user with ID ${userId}`);
     const user = await getUserById(userId);
     if (!user) {
+      logger.warn(`User with ID ${userId} not found`);
       return next(new ApiError(404, "User not found"));
     }
 
+    logger.info(`User details fetched successfully for user with ID ${userId}`);
     res.status(200).json(user);
   } catch (error) {
+    const err = error as Error;
+    logger.error(
+      `Error fetching user details for user with ID ${userId}: ${err.message}`
+    );
     next(new ApiError(500, "Error fetching user details"));
+  } finally {
+    logger.info("userController.getUserDetailsController(): Function Exit");
   }
 };
 
@@ -36,17 +48,21 @@ export const updateUserDetailsController = async (
   res: Response,
   next: NextFunction
 ) => {
+  logger.info("userController.updateUserDetailsController(): Function Entry");
+
   const userId = req.user.id;
   const updatedFields = req.body;
 
   try {
-    // Check if user exists
+    logger.info(`Checking if user with ID ${userId} exists`);
     const user = await getUserById(userId);
     if (!user) {
+      logger.warn(`User with ID ${userId} not found`);
       return next(new ApiError(404, "User not found"));
     }
 
     // Validate the fields
+    logger.info(`Validating update fields for user with ID ${userId}`);
     const fields = Object.keys(updatedFields);
     const isValidOperation = fields.every((field) =>
       allowedFields.includes(field)
@@ -56,21 +72,32 @@ export const updateUserDetailsController = async (
       const invalidFields = fields
         .filter((field) => !allowedFields.includes(field))
         .join(", ");
+      logger.warn(
+        `Invalid fields to update for user with ID ${userId}: ${invalidFields}`
+      );
       return next(
         new ApiError(400, `Invalid fields to update: ${invalidFields}`)
       );
     }
+
     // Restricted fields that cannot be updated
     const restrictedFields = ["account_number", "email"];
 
     // Check for restricted fields
     validateUpdateFields(fields, restrictedFields, next);
 
+    logger.info(`Updating user details for user with ID ${userId}`);
     await updateUser(userId, updatedFields);
+    logger.info(`User details updated successfully for user with ID ${userId}`);
     res.status(200).json({ message: "User updated successfully" });
   } catch (error) {
-    console.log("errir", error);
+    const err = error as Error;
+    logger.error(
+      `Error updating user details for user with ID ${userId}: ${err.message}`
+    );
     next(new ApiError(500, "Error updating user details"));
+  } finally {
+    logger.info("userController.updateUserDetailsController(): Function Exit");
   }
 };
 
@@ -80,18 +107,28 @@ export const deleteUserController = async (
   res: Response,
   next: NextFunction
 ) => {
+  logger.info("userController.deleteUserController(): Function Entry");
+
   const userId = req.user.id;
 
   try {
+    logger.info(`Checking if user with ID ${userId} exists`);
     const user = await getUserById(userId);
     if (!user) {
+      logger.warn(`User with ID ${userId} not found`);
       return next(new ApiError(404, "User not found"));
     }
 
+    logger.info(`Deleting user with ID ${userId}`);
     await deleteUser(userId);
+    logger.info(`User deleted successfully with ID ${userId}`);
     res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
+    const err = error as Error;
+    logger.error(`Error deleting user with ID ${userId}: ${err.message}`);
     next(new ApiError(500, "Error deleting user"));
+  } finally {
+    logger.info("userController.deleteUserController(): Function Exit");
   }
 };
 
@@ -101,31 +138,55 @@ export const getAllUsersController = async (
   res: Response,
   next: NextFunction
 ) => {
+  logger.info("userController.getAllUsersController(): Function Entry");
+
   try {
+    logger.info("Fetching all users");
     const users = await getAllUsers();
+    logger.info("All users fetched successfully");
     res.status(200).json(users);
   } catch (error) {
+    const err = error as Error;
+    logger.error(`Error fetching all users: ${err.message}`);
     next(new ApiError(500, "Error fetching users"));
+  } finally {
+    logger.info("userController.getAllUsersController(): Function Exit");
   }
 };
 
 // Controller to get users name with account number in params
-export const getUserByAccountNumberontroller = async (
+export const getUserByAccountNumberController = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  logger.info(
+    "userController.getUserByAccountNumberController(): Function Entry"
+  );
+
   const { accountNumber } = req.params;
   try {
-    const users = await getUserByAccountNumber(accountNumber);
-    if (!users) {
+    logger.info(`Fetching user with account number ${accountNumber}`);
+    const user = await getUserByAccountNumber(accountNumber);
+    if (!user) {
+      logger.warn(`User with account number ${accountNumber} not found`);
       return next(new ApiError(404, "User not found"));
     }
+    logger.info(
+      `User with account number ${accountNumber} fetched successfully`
+    );
     res
       .status(200)
-      .json({ name: users.name, accountNumber: users.account_number });
+      .json({ name: user.name, accountNumber: user.account_number });
   } catch (error) {
-    console.log("err", error);
+    const err = error as Error;
+    logger.error(
+      `Error fetching user with account number ${accountNumber}: ${err.message}`
+    );
     next(new ApiError(500, "Error fetching users"));
+  } finally {
+    logger.info(
+      "userController.getUserByAccountNumberController(): Function Exit"
+    );
   }
 };
