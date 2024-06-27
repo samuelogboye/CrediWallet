b95bfe4475d9c2aba9b9a65de018d3c117a3ee17
Samuel Ogboye <ogboyesam@gmail.com>
2024-06-27 20:39:55 +0100

    feat(test): implemented testing for base

---
+++ tests/base.test.ts
@@ -0,0 +1,11 @@
+process.env.NODE_ENV = "test";
+
+import request from "supertest";
+import app from "../src/app";
+
+describe("Test app.ts", () => {
+  test("Catch-all route", async () => {
+    const res = await request(app).get("/");
+    expect(res.body).toEqual({ message: "Welcome to CrediWallet" });
+  });
+});
