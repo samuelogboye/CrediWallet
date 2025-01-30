23fdee6bc0151df088faa0dde0def256046beecb
Samuel Ogboye <ogboyesam@gmail.com>
2025-01-30 23:47:48 +0100

    refactor: clean up auth routes comments

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -8,10 +8,8 @@ import { validateRegister, validateLogin } from "../utils/validator";
 
 const router = Router();
 
-// Route to register a new user
 router.post("/register", validateRegister, checkBlacklist, registerController);
 
-// Route to log in an existing user
 router.post("/login", validateLogin, loginController);
 
 export default router;
